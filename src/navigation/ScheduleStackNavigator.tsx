7f33fad8912521c0e7a80345c72890a4a23ba11a
김젼득 <wusemr2@naver.com>
2025-05-19 23:58:12 +0900

    refactor: Schedule 탭에 StackNavigator 도입 및 Tab/Stack 네비게이션 구조 개편
    - Schedule 탭 내 루틴/할일 작성 화면 이동을 위해 ScheduleStackNavigator.tsx 추가
    - TabNavigation에서 ScheduleStackNavigator 연동
    - 전체 네비게이션 구조(StackNavigation) 일부 조정

---
+++ src/navigation/ScheduleStackNavigator.tsx
@@ -0,0 +1,35 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import {
+    ScheduleScreen,
+    CalendarScreen,
+    RoutineFormScreen,
+    RoutineScreen,
+    TaskListFormScreen,
+    TaskListScreen
+} from '../screens'
+
+export type ScheduleStackParamList = {
+    ScheduleScreen: undefined,
+    CalendarScreen: undefined,
+    RoutineFormScreen: undefined,
+    RoutineScreen: undefined,
+    TaskListFormScreen: undefined,
+    TaskListScreen: undefined
+}
+
+const Stack = createStackNavigator<ScheduleStackParamList>()
+
+const ScheduleStackNavigator = () => {
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="ScheduleScreen" component={ScheduleScreen} />
+            <Stack.Screen name="CalendarScreen" component={CalendarScreen} />
+            <Stack.Screen name="RoutineFormScreen" component={RoutineFormScreen} />
+            <Stack.Screen name="RoutineScreen" component={RoutineScreen} />
+            <Stack.Screen name="TaskListFormScreen" component={TaskListFormScreen} />
+            <Stack.Screen name="TaskListScreen" component={TaskListScreen} />
+        </Stack.Navigator>
+    )
+}
+
+export default ScheduleStackNavigator
