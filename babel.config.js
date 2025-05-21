80cdcd33fe42ec5b5f73db88c4794c8defbe7f5c
ChoiJaeYeon23 <wasid4598@gmail.com>
2025-05-21 23:39:49 +0900

    chore: 가계부 기본 구성 추가

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['@react-native/babel-preset'],
   plugins: [
     'react-native-reanimated/plugin', // 이거 꼭 마지막 줄에 위치해야 함
   ],
