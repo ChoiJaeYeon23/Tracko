a318fe20c03be4b972cc227d3540b2acbd151e21
김젼득 <wusemr2@naver.com>
2025-05-21 23:36:22 +0900

    chore: 라이브러리 추가 및 변경에 따른 의존성 파일 업데이트

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['@react-native/babel-preset'],
   plugins: [
-    'react-native-reanimated/plugin', // 이거 꼭 마지막 줄에 위치해야 함
+    'react-native-reanimated/plugin',
   ],
 };
